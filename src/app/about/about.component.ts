8d2d86b4e71487d6ae1a91d6dda7a13cb46b9e00
Basith <basith@getmeroof.com>
2019-06-11 14:59:30 +0530

    conter

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
 
   ngOnInit() {
+    if(isPlatformBrowser(this.platformId)) {
+        $(document).ready(function(){
+        var a = 0;
+        $(window).scroll(function() {        
+          var oTop = $('#counter').offset().top - window.innerHeight;
+          if (a == 0 && $(window).scrollTop() > oTop) {
+            $('.stat-number').each(function () {
+              var size = $(this).text().split(",")[1] ? $(this).text().split(",")[1].length : 0;
+              $(this).prop('Counter', 0).animate({
+                 Counter: $(this).text()
+              }, {
+                 duration: 2500,
+                 step: function (func:any) {
+                    $(this).text(parseFloat(func).toFixed(size));
+                 },
+                 complete: function() {
+                   var val= $(this).text();
+                   var cVal = function formatNumber(val) {
+                    return val.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+                  }
+                  
+                  $(this).text(cVal(val)+'+');
+                  
+                 }
+              });            
+           });      
+            a = 1;
+          }         
+        });  
+      })
+
+    }
   }
 
 }
