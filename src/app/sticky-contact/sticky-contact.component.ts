4bf156a050c392f1a86b1d5ca501b1efee48bc79
Basith <basith@getmeroof.com>
2019-06-19 18:23:46 +0530

    contact back

--- src/app/sticky-contact/sticky-contact.component.ts
+++ src/app/sticky-contact/sticky-contact.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 
 import { ToastrService } from 'ngx-toastr';
+import { environment } from '../../environments/environment'
 
 @Component({
   selector: 'app-sticky-contact',
@@ -9,11 +11,18 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class StickyContactComponent implements OnInit {
 
-  constructor(private toastr: ToastrService) { }
+  constructor(private toastr: ToastrService,private http: HttpClient) { }
 
   _opened: boolean;
   position:any;
   animate:any;
+
+  data = {
+    name: '',
+    email: '',
+    number: '',
+    message: ''
+  }
 
   ngOnInit() {
     this.position = 'bottom'
@@ -26,11 +35,26 @@ export class StickyContactComponent implements OnInit {
   }
 
   post(){
-    this.toastr.error('Our server is down at this moment, please try agin after some time.','Error', {
-      positionClass: 'toast-top-center',
-      progressBar: true,
-      timeOut: 5000
-    });
+   return this.http.post(environment.apiUrl+'/contact',this.data).subscribe(
+
+    res => {
+      this._opened = !this._opened;
+      this.toastr.success('Your Response Has Been Recorded. We will Get Back To You Shortly.', 'Successful',{
+        positionClass: 'toast-top-center',
+        progressBar: true,
+        timeOut: 5000
+      });
+
+    },
+
+    err => {
+      this.toastr.error('Our server is down at this moment, please try agin after some time.','Error', {
+        positionClass: 'toast-top-center',
+        progressBar: true,
+        timeOut: 5000
+      });
+    }
+   );
   }
 
 }
